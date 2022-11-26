960204cb0ce0cd43b443876db5d5eee95c5ab4be
ohadsgithub <90630277+ohadsgithub@users.noreply.github.com>
2022-11-26 09:49:48 +0200

    Update image_demo_directModel1.py

--- demo/image_demo_directModel1.py
+++ demo/image_demo_directModel1.py
@@ -5,8 +5,24 @@ from mmseg.core.evaluation import get_palette
 
 import numpy as np
 
+import os
+# from tqdm import tqdm
+#from tqdm.notebook import tqdm
+from PIL import Image
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+#from data.base_dataset import Normalize_image
+#from utils.saving_utils import load_checkpoint_mgpu
+
 
 def main():
+    
+    device = 'cuda'
+    
     parser = ArgumentParser()
     parser.add_argument('img', help='Image file')
     parser.add_argument('config', help='Config file')
@@ -30,6 +46,7 @@ def main():
     model = init_segmentor(args.config, args.checkpoint, device=args.device)
     
     # test a single image
+    '''
     #result = inference_segmentor(model, args.img)
     result = inference_segmentor(model, False, args.img)
     
@@ -38,22 +55,75 @@ def main():
     
     print(type(result))
     arr_2 = np.array(result)
-    print(arr_2.shape)
+    print(arr_2.shape)'''
     
     # show the results
+    '''
     show_result_pyplot(
         model,
         args.img,
         result,
         get_palette(args.palette),
         opacity=args.opacity,
-        out_file=args.out_file)
+        out_file=args.out_file)'''
     
-    print(type(result))
-    #print(result.size())
-    arr_2 = np.array(result)
-    print(arr_2.shape)
+    transforms_list = []
+    transforms_list += [transforms.ToTensor()]
+    transforms_list += [Normalize_image(0.5, 0.5)]
+    transform_rgb = transforms.Compose(transforms_list)
+    
+    '''
+    net = U2NET(in_ch=3, out_ch=4)
+    net = load_checkpoint_mgpu(net, checkpoint_path)
+    net = net.to(device)'''
+    #net = model
+    model = model.eval()
 
+    palette = get_palette_cloths(4)
+    
+    #img = Image.open(os.path.join(image_dir, image_name)).convert('RGB')
+    img = Image.open(args.img).convert('RGB')
+    img_size = img.size
+    #img = img.resize((768, 768), Image.BICUBIC)######################################################################
+    image_tensor = transform_rgb(img)
+    image_tensor = torch.unsqueeze(image_tensor, 0)
+    
+    output_tensor = model(image_tensor.to(device))
+    '''
+    output_tensor = F.log_softmax(output_tensor[0], dim=1)
+    output_tensor = torch.max(output_tensor, dim=1, keepdim=True)[1]
+    output_tensor = torch.squeeze(output_tensor, dim=0)
+    output_tensor = torch.squeeze(output_tensor, dim=0)
+    '''
+    output_arr = output_tensor.cpu().numpy()
+    
+    print(type(output_tensor))
+    print(type(output_arr))
+    #print(result.size())
+    print(output_arr.shape)
+    
+def get_palette_cloths(num_cls):
+    """ Returns the color map for visualizing the segmentation mask.
+    Args:
+        num_cls: Number of classes
+    Returns:
+        The color map
+    """
+    n = num_cls
+    palette = [0] * (n * 3)
+    for j in range(0, n):
+        lab = j
+        palette[j * 3 + 0] = 0
+        palette[j * 3 + 1] = 0
+        palette[j * 3 + 2] = 0
+        i = 0
+        while lab:
+            palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+            palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+            palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+            i += 1
+            lab >>= 3
+    return palette
 
 if __name__ == '__main__':
     main()
