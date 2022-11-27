ec39925807e1bcf7c59817544941863587966e4a
ohadsgithub <90630277+ohadsgithub@users.noreply.github.com>
2022-11-27 22:35:46 +0200

    Update __init__.py

--- mmseg/apis/__init__.py
+++ mmseg/apis/__init__.py
@@ -7,5 +7,5 @@ from .train import (get_root_logger, init_random_seed, set_random_seed,
 __all__ = [
     'get_root_logger', 'set_random_seed', 'train_segmentor', 'init_segmentor',
     'inference_segmentor', 'multi_gpu_test', 'single_gpu_test',
-    'show_result_pyplot', 'init_random_seed'
+    'show_result_pyplot', 'show_result_pyplot2', 'init_random_seed'
 ]
