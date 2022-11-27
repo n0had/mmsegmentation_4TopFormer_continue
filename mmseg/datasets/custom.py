d63865c606c642c6d26b88d186b2f3b973a14d6f
ohadsgithub <90630277+ohadsgithub@users.noreply.github.com>
2022-11-27 12:00:24 +0200

    Update custom.py

--- mmseg/datasets/custom.py
+++ mmseg/datasets/custom.py
@@ -228,7 +228,11 @@ class CustomDataset(Dataset):
         img_info = self.img_infos[idx]
         ann_info = self.get_ann_info(idx)
         results = dict(img_info=img_info, ann_info=ann_info)
+        
+        print("\n\n dictionary info")
         print(results)
+        print("\n\n")
+        
         self.pre_pipeline(results)
         return self.pipeline(results)
 
